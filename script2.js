eef7071e3374b5a0c997dd1b28315248737f0fed
gelory <917589697@qq.com>
2018-06-09 12:06:26 +0800

    轮播最终版

--- script2.js
+++ script2.js
@@ -1,28 +1,80 @@
-//初始化
-let n;
-start();
-setInterval(function(){
-    $(`.imgs img:nth-child(${xxx(n)})`).addClass('leave').removeClass('current')
-    .one('transitionend',function(x){
-        $(x.currentTarget).addClass('enter').removeClass('leave');
-    })
-    $(`.imgs img:nth-child(${xxx(n+1)})`).addClass('current').removeClass('enter');
-    n+=1;
-},1000)
+//初始化 
+let $imgs = $('#imgs > img');
+let fistcopy = $imgs.eq(0).clone(true)[0];
+let lastcopy = $imgs.eq($imgs.length-1).clone(true)[0];
+$('#imgs').prepend($(lastcopy)).append($(fistcopy)).css({transform:'translateX(-200px)'});
 
-function xxx(n){
-    if(n>4){
-        n=n%4;
-        if(n===0){
-            n=4;
-        }
+//增加鼠标点击事件
+
+$buttons = $('#buttonWrap > button')
+let current=0;
+$buttons.on('click',function(e){
+    index =$(e.currentTarget).index();
+    timeStep(index);
+})
+
+//加上定时器自动播放
+let index;
+var timer = setInterval(function(){
+    if(index>$imgs.length-1||index===undefined){
+        index=0;
     }
-    return n
+    timeStep(index);
+    index+=1;
+},1000)
+$imgs.on('mouseenter',function(){
+    window.clearInterval(timer);
+})
+$imgs.on('mouseleave',function(){
+    timer = setInterval(function(){
+        if(index>$imgs.length-1||index===undefined){
+            index=0;
+        }
+        timeStep(index);
+        index+=1;
+    },1000)
+})
+
+//监听用户是否从轮播页面切换到其他页面
+document.addEventListener('visibilitychange',function(){
+    if(document.hidden){
+        window.clearInterval(timer);
+    }else{
+        timer = setInterval(function(){
+            if(index>$imgs.length-1||index===undefined){
+                index=0;
+            }
+            timeStep(index);
+            index+=1;
+        },1000)
+    }
+});
+
+//函数
+function timeStep(index){
+    if(current === $buttons.length-1 && index===0){
+        jump(current+2,index+1);    
+    }else if(current === 0 && index===$buttons.length-1){
+        jump(current,index+1);    
+    }else{ 
+        trans(index+1);  
+    }
+    current = index;
 }
-function start(){
-    n=1;
-    $('.imgs img:nth-child(1)').addClass('current');
-    $('.imgs img:nth-child(2)').addClass('enter');
-    $('.imgs img:nth-child(3)').addClass('enter');
-    $('.imgs img:nth-child(4)').addClass('enter'); 
+function trans(n){
+    $('#imgs').css({transform:`translateX(-${(n)*200}px)`});
+    $(`#buttonWrap > button`).removeClass('active');
+    if(n===$buttons.length+1){
+        n=1;
+    }else if(n===0){
+        n=$buttons.length;
+    }
+    $(`#buttonWrap > button:nth(${n-1})`).addClass('active');
+}
+function jump(a,b){
+    trans(a);
+    $('#imgs').one('transitionend',function(e){
+        $('#imgs').hide().css({transform:`translateX(-${(b)*200}px)`}).offset();
+        $('#imgs').show();
+    })
 }
